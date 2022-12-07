90f4060b39bee980347a73293af579241315f7c3
Mubin Ansari <mubinansari910@gmail.com>
2022-12-07 19:57:22 +0530

    add firebase

---
+++ firebase.example.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'yourconfigdetailshere',
+  authDomain: 'yourconfigdetailshere',
+  projectId: 'yourconfigdetailshere',
+  storageBucket: 'yourconfigdetailshere',
+  messagingSenderId: 'yourconfigdetailshere',
+  appId: 'yourconfigdetailshere',
+};
