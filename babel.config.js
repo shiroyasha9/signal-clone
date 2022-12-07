4797b81a79b811f9a014c5be5f3353033f079003
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 00:58:44 +0530

    add support for aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './assets',
+            '@components': './components',
+            '@constants': './constants',
+            '@hooks': './hooks',
+            '@navigators': './navigators',
+            '@screens': './screens',
+            '@stores': './stores',
+            '@themes': './themes',
+            '@types': './types',
+            '@utils': './utils',
+          },
+        },
+      ],
+    ],
   };
 };
