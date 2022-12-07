77a2b5eba13a47c91dfb8c9d9f55bf6c70feef8c
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 01:09:15 +0530

    customize the navigator header

--- navigators/StackNavigator.tsx
+++ navigators/StackNavigator.tsx
@@ -1,12 +1,19 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { LoginScreen } from '@screens';
+import { Colors } from '@themes';
 import { INavigatorParamList } from '@types';
 
 const Stack = createStackNavigator<INavigatorParamList>();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.royalBlue },
+        headerTitleStyle: { color: Colors.white },
+        headerTintColor: Colors.white,
+      }}
+    >
       <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
---
+++ themes/palette.ts
@@ -0,0 +1,4 @@
+export const Colors = {
+  royalBlue: '#2C6BED',
+  white: '#FFFFFF',
+};
