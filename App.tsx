a88d9aabb193c814810138f6dfa8e49497db8b57
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 01:10:04 +0530

    add and configure react native elements package

--- App.tsx
+++ App.tsx
@@ -2,18 +2,19 @@ import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import StackNavigator from '@navigators/StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
+    <SafeAreaProvider>
+      <StatusBar style="light" />
       <NavigationContainer>
         <StackNavigator />
       </NavigationContainer>
-    </>
+    </SafeAreaProvider>
   );
 }
 
