1f694d0a2e63ee831a18f5f149d0547fbd14ea69
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 14:48:26 +0530

    add basic signout functionality to the home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,41 @@
+import { signOut } from 'firebase/auth';
+import { useAtom } from 'jotai';
+import { useLayoutEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const HomeScreen = () => {
+import { StackScreenProps } from '@react-navigation/stack';
+import { Button } from '@rneui/base';
+import { userAtom } from '@stores';
+import { Colors } from '@themes';
+import { NavigatorParamList } from '@types';
+
+import { auth } from '../firebase';
+
+type HomeScreenProps = StackScreenProps<NavigatorParamList, 'Home'>;
+
+export const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+  const [user, setUser] = useAtom(userAtom);
+  const logoutHandler = () => {
+    signOut(auth).then(() => {
+      setUser(null);
+      navigation.navigate('Login');
+    });
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          title="Logout"
+          color={Colors.royalBlue}
+          onPress={logoutHandler}
+        />
+      ),
+    });
+  }, [navigation]);
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Welcome Home, {user?.displayName}</Text>
     </View>
   );
 };
