13f7cb547ac7a2bdb4b0da10ec906078bf89be4f
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 14:48:15 +0530

    add ignore log for redundant asyncstorage warning

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { useAtomValue } from 'jotai';
 import { Suspense } from 'react';
-import { Text } from 'react-native';
+import { LogBox, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import StackNavigator from '@navigators/StackNavigator';
@@ -11,12 +11,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { userAtom } from '@stores';
 import { NavigatorParamList } from '@types';
 
+// Extend RootParamList to ensure type safety
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends NavigatorParamList {}
   }
 }
 
+// Ignore AsyncStorage warning (comes from Expo)
+LogBox.ignoreLogs([
+  'AsyncStorage has been extracted from react-native core and will be removed in a future release.',
+]);
+
+// Preload neccessary atoms to use on app mount
 const Preloader = () => {
   useAtomValue(userAtom); // The value will be pre-loaded
   return null;
