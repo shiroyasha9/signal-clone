7bc101ad1c7740a1974931dd0b43416122b21083
Mubin Ansari <mubinansari910@gmail.com>
2022-12-08 13:31:59 +0530

    integrate firebase auth and db

--- firebase.example.ts
+++ firebase.example.ts
@@ -1,3 +1,7 @@
+import { FirebaseApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 export const firebaseConfig = {
   apiKey: 'yourconfigdetailshere',
   authDomain: 'yourconfigdetailshere',
@@ -6,3 +10,16 @@ export const firebaseConfig = {
   messagingSenderId: 'yourconfigdetailshere',
   appId: 'yourconfigdetailshere',
 };
+
+let app: FirebaseApp;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
