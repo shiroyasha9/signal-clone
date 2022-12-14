d6365c85f9ead6aaaf755f828df36dc08de2419d
Mubin Ansari <mubinansari910@gmail.com>
2022-12-14 16:27:25 +0530

    add validation to login using formik and yup

---
+++ components/LoginForm.tsx
@@ -0,0 +1,108 @@
+import { Formik, FormikProps } from 'formik';
+import { GestureResponderEvent, StyleSheet, View } from 'react-native';
+import * as Yup from 'yup';
+
+import { Button, Input } from '@rneui/base';
+import { Colors } from '@themes';
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
+
+type LoginFormProps = {
+  loginHandler: (
+    values: LoginFormValues,
+    resetSubmitHandler: () => void,
+  ) => void;
+};
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().required().email(),
+  password: Yup.string().min(6).max(50).required(),
+});
+
+export const LoginForm = (props: LoginFormProps) => {
+  const { loginHandler } = props;
+
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={LoginSchema}
+      onSubmit={(values, { setSubmitting }) =>
+        loginHandler(values, () => setSubmitting(false))
+      }
+      validateOnMount
+    >
+      {(props) => <LoginInnerForm {...props} />}
+    </Formik>
+  );
+};
+
+const LoginInnerForm = (props: FormikProps<LoginFormValues>) => {
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    isValid,
+  } = props;
+
+  return (
+    <View style={styles.inputContainer}>
+      <Input
+        label="Email"
+        placeholder="rickroll@example.com"
+        onChangeText={handleChange('email')}
+        onBlur={handleBlur('email')}
+        value={values.email}
+        autoCorrect={false}
+        autoCapitalize="none"
+        errorMessage={
+          errors.email && touched.email
+            ? 'Please enter a valid email address.'
+            : undefined
+        }
+      />
+      <Input
+        label="Password"
+        placeholder="******"
+        onChangeText={handleChange('password')}
+        onBlur={handleBlur('password')}
+        value={values.password}
+        secureTextEntry
+        errorMessage={
+          errors.password && touched.password
+            ? 'Please enter a valid password.'
+            : undefined
+        }
+      />
+
+      <Button
+        onPress={
+          handleSubmit as unknown as (event: GestureResponderEvent) => void
+        }
+        title="Submit"
+        containerStyle={styles.button}
+        color={Colors.royalBlue}
+        disabled={isSubmitting || !isValid}
+        loading={isSubmitting}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: 300,
+    marginTop: 40,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+});
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,10 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useSetAtom } from 'jotai';
-import { useState } from 'react';
-import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 
+import { LoginForm, Screen } from '@components';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Button, Image, Input } from '@rneui/themed';
+import { Image, Text } from '@rneui/base';
 import { userAtom } from '@stores';
 import { Colors } from '@themes';
 import { NavigatorParamList } from '@types';
@@ -14,12 +14,13 @@ import { auth } from '../firebase';
 type LoginScreenProps = StackScreenProps<NavigatorParamList, 'Login'>;
 
 export const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const setUser = useSetAtom(userAtom);
 
-  const loginHandler = () => {
-    signInWithEmailAndPassword(auth, email, password)
+  const loginHandler = (
+    values: { email: string; password: string },
+    resetSubmitHandler: () => void,
+  ) => {
+    signInWithEmailAndPassword(auth, values.email, values.password)
       .then((userCredential) => {
         const user = userCredential.user;
         setUser(user);
@@ -31,7 +32,8 @@ export const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
       .catch((error) => {
         const errorMessage = error.message;
         alert(errorMessage);
-      });
+      })
+      .finally(() => resetSubmitHandler());
   };
 
   const registerHandler = () => {
@@ -39,39 +41,11 @@ export const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
   };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    >
-      <Image
-        source={{
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Signal-Logo.svg/600px-Signal-Logo.svg.png',
-        }}
-        style={{ width: 200, height: 200, borderRadius: 25 }}
-      />
-      <View style={styles.inputContainer}>
-        <Input
-          label="Email"
-          placeholder="rickroll@example.com"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Input
-          label="Password"
-          placeholder="******"
-          secureTextEntry
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-      <Button
-        title="Login"
-        containerStyle={styles.button}
-        onPress={loginHandler}
-        color={Colors.royalBlue}
-      />
+    <Screen style={styles.container}>
+      <Image source={require('@assets/logo.png')} style={styles.logo} />
+
+      <LoginForm loginHandler={loginHandler} />
+
       <View style={styles.registerText}>
         <Text>Not an existing user? </Text>
         <Pressable
@@ -81,31 +55,26 @@ export const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
           <Text style={{ color: Colors.royalBlue }}>Register</Text>
         </Pressable>
       </View>
+
       <View style={{ height: 120 }} />
-    </KeyboardAvoidingView>
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 10,
-    backgroundColor: Colors.white,
-  },
-  inputContainer: {
-    width: 300,
-    marginTop: 40,
-  },
-  button: {
-    width: 200,
-    marginTop: 10,
   },
   registerText: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 15,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    borderRadius: 25,
   },
 });
